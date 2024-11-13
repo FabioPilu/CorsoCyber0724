5ddeb9964e90653eb84971f112ea7c3125c59364
FabioPilu <fabio.pilu93@gmail.com>
2024-11-13 17:41:15 +0100

    Ancora qualche problemino
    Sto avendo ancora problemi con la condivisione da kali, stasera cercherò di risolvere l'intoppo

---
+++ Corso Cyber S2/Esercitazione13nov.py
@@ -0,0 +1,8 @@
+print("Ciao!")
+
+citta = input("Inserisci il nome della tua città: ")
+animale = input("Inserisci il nome del tuo animale domestico: ")
+
+nome_band = citta+""+animale
+
+print(f"il nome della tua band può essere: {nome_band}")
